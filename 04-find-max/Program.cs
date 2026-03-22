bef78b1d076152221118efb12ce8e182c014ce21
Mohamed Mahmoud Hussien <mmhshika6@gmail.com>
2026-03-22 18:54:01 +0200

    Add solution and README for 04 - Find max number in array

--- 04-find-max/Program.cs
+++ 04-find-max/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Enter array elements separated by comma ( , ): ");
+int max = int.MinValue;
+// read input and check for null, split by comma, convert them to int, store in array
+string? input = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(input))
+{
+    int[] numbers = input.Split(',')
+                    .Select(int.Parse)
+                    .ToArray();
+
+    // finding the max element then print it
+    foreach (int num in numbers)
+    {
+        if (num > max)
+            max=num;
+    }
+    System.Console.WriteLine($"The max element is {max}");
+   
+}
+else
+{
+    // array is empty 
+    System.Console.WriteLine("The array is empty");
+}
---
+++ 04-find-max/README.md
@@ -0,0 +1,31 @@
+# 04 - Find Maximum
+
+## Description
+
+Write a program that finds the maximum value in an array of integers.
+
+## Input
+
+- An array of integers
+
+## Output
+
+- A single integer representing the largest value in the array
+
+## Example
+
+Input:  
+[3, 7, 2, 9, 5]  
+
+Output:  
+9  
+
+Input:  
+[-1, -5, -3]  
+
+Output:  
+-1  
+
+## Notes
+
+- The array may contain negative numbers  
