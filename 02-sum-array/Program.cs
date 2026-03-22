e321591a2200755a8c868329ee6100b9d1337469
Mohamed Mahmoud Hussien <mmhshika6@gmail.com>
2026-03-22 17:52:49 +0200

    Add solution and README for 02 - Sum Array Elements

--- 02-sum-array/Program.cs
+++ 02-sum-array/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Enter array elements separated by comma ( , ): ");
+int sum = 0;
+// read input and check for null, split by comma, convert them to int, store in array
+string? input = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(input))
+{
+    int[] numbers = input.Split(',')
+                    .Select(int.Parse)
+                    .ToArray();
+
+    // print array elements and calculating the sum
+    Console.WriteLine("Array elements are:");
+    foreach (int num in numbers)
+    {
+        Console.Write(num + " ");
+        sum += num;
+    }
+}
+else
+{
+    // if array is empty then sum = 0 
+    sum = 0;
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Sum of all elements = {sum}");
---
+++ 02-sum-array/README.md
@@ -0,0 +1,32 @@
+# 02 - Sum Array Elements
+
+## Description
+
+Write a program that calculates the sum of all elements in an array of integers.
+
+## Input
+
+- An array of integers
+
+## Output
+
+- A single integer representing the sum of all elements
+
+## Example
+
+Input:  
+[1, 2, 3, 4, 5]  
+
+Output:  
+15  
+
+Input:  
+[-2, 5, 7]  
+
+Output:  
+10  
+
+## Notes
+
+- The array may contain negative numbers  
+- If the array is empty, the sum should be 0
