54892b711576c9ee8ecf03ef1913508b68d435ca
Mohamed Mahmoud Hussien <mmhshika6@gmail.com>
2026-03-22 18:59:02 +0200

    Add solution and README for 05 - Find minimum element in array

--- 05-find-min/Program.cs
+++ 05-find-min/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Enter array elements separated by comma ( , ): ");
+int min = int.MaxValue;
+// read input and check for null, split by comma, convert them to int, store in array
+string? input = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(input))
+{
+    int[] numbers = input.Split(',')
+                    .Select(int.Parse)
+                    .ToArray();
+
+    // finding the min element then print it
+    foreach (int num in numbers)
+    {
+        if (num < min)
+            min = num;
+    }
+    System.Console.WriteLine($"The minimum element is {min}");
+
+}
+else
+{
+    // array is empty 
+    System.Console.WriteLine("The array is empty");
+}
---
+++ 05-find-min/README.md
@@ -0,0 +1,32 @@
+# 05 - Find Minimum
+
+## Description
+
+Write a program that finds the minimum value in an array of integers.
+
+## Input
+
+- An array of integers
+
+## Output
+
+- A single integer representing the smallest value in the array
+
+## Example
+
+Input:  
+[3, 7, 2, 9, 5]  
+
+Output:  
+2  
+
+Input:  
+[-1, -5, -3]  
+
+Output:  
+-5  
+
+## Notes
+
+- The array may contain negative numbers  
+- Assume the array contains at least one element
