77f12bf2e4f8ee97efa733f0b5cd634608988114
Mohamed Mahmoud Hussien <mmhshika6@gmail.com>
2026-03-22 18:38:43 +0200

    Add solution and README for 03 - Find average

--- 03-find-average/Program.cs
+++ 03-find-average/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Enter array elements separated by comma ( , ): ");
+int sum = 0;
+float avg = 0;
+// read input and check for null, split by comma, convert them to int, store in array
+string? input = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(input))
+{
+    int[] numbers = input.Split(',')
+                    .Select(int.Parse)
+                    .ToArray();
+
+    // print array elements and calculating the sum and avg
+    Console.WriteLine("Array elements are:");
+    foreach (int num in numbers)
+    {
+        Console.Write(num + " ");
+        sum += num;
+    }
+    avg = (float)sum / numbers.Length;
+}
+else
+{
+    // if array is empty then sum = 0 and avg = 0
+    sum = 0;
+    avg = 0;
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Sum of all elements = {sum}");
+System.Console.WriteLine($"Average = {avg}");
---
+++ 03-find-average/README.md
@@ -0,0 +1,32 @@
+# 03 - Find Average
+
+## Description
+
+Write a program that calculates the average of all elements in an array of integers.
+
+## Input
+
+- An array of integers
+
+## Output
+
+- A single number representing the average of all elements
+
+## Example
+
+Input:  
+[2, 4, 6, 8]  
+
+Output:  
+5.0  
+
+Input:  
+[1, 3, 5]  
+
+Output:  
+3.0  
+
+## Notes
+
+- The array may contain negative numbers  
+- If the array is empty, the average should be 0
