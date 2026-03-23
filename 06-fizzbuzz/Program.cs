e5ec5a38b7070b66769166154777acd212ef2ad9
Mohamed Mahmoud Hussien <mmhshika6@gmail.com>
2026-03-23 19:02:00 +0200

    Add solution and README for 06 - FizzBuzz

--- 06-fizzbuzz/Program.cs
+++ 06-fizzbuzz/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? input;
+int number;
+do
+{
+    System.Console.Write("Enter postive integer: ");
+    input = Console.ReadLine() ?? "";
+
+} while (!(int.TryParse(input, out number) && number > 0));
+
+for (int i = 1; i <= number; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        System.Console.WriteLine("FizzBuzz");
+    else if (i % 3 == 0)
+        System.Console.WriteLine("Fizz");
+    else if (i % 5 == 0)
+        System.Console.WriteLine("Buzz");
+    else
+        System.Console.WriteLine(i);
+}
---
+++ 06-fizzbuzz/README.md
@@ -0,0 +1,42 @@
+# 06 - FizzBuzz
+
+## Description
+
+Print numbers from 1 to N.  
+- For multiples of 3, print "Fizz"  
+- For multiples of 5, print "Buzz"  
+- For multiples of both 3 and 5, print "FizzBuzz"
+
+## Input
+
+- A single integer N
+
+## Output
+
+- Numbers from 1 to N following the FizzBuzz rules
+
+## Example
+
+Input:  
+15  
+
+Output:  
+1  
+2  
+Fizz  
+4  
+Buzz  
+Fizz  
+7  
+8  
+Fizz  
+Buzz  
+11  
+Fizz  
+13  
+14  
+FizzBuzz  
+
+## Notes
+
+- N is a positive integer
