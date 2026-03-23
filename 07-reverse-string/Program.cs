16324aafd0e5839712b9902a880ee889b4f2cfd2
Mohamed Mahmoud Hussien <mmhshika6@gmail.com>
2026-03-23 21:20:14 +0200

    Add solution and README for 07 - Reverse String

--- 07-reverse-string/Program.cs
+++ 07-reverse-string/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input, reversedString = "";
+
+
+// take the string as input
+System.Console.Write("Enter String to reverse: ");
+input = Console.ReadLine() ?? "";
+//System.Console.WriteLine(input.Length);
+
+if (input.Length > 0)
+{
+    int inputLength = input.Length;
+    for (int i = inputLength - 1; i >= 0; i--)
+    {
+        reversedString += input[i];
+    }
+    System.Console.WriteLine(reversedString);
+}
---
+++ 07-reverse-string/README.md
@@ -0,0 +1,31 @@
+# 07 - Reverse String
+
+## Description
+
+Write a program that reverses a given string.
+
+## Input
+
+- A single string
+
+## Output
+
+- The reversed string
+
+## Example
+
+Input:  
+Hello World  
+
+Output:  
+dlroW olleH  
+
+Input:  
+CSharp  
+
+Output:  
+prahSC  
+
+## Notes
+
+- Preserve spaces and case
